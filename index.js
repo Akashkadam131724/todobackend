ee058c1747e531729a7d9c6a6d3d9a2a5b92e308
Akashkadam131724 <kadamakash131724@gmail.com>
2024-01-29 14:50:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!2");
+});
+app.get("/twitter", (req, res) => {
+  res.send("Hello Twitter");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>Hello Login page</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
